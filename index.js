53c274db9e9c643d2547dbbb656160f7e54a8670
Cameron Spencer <cameron@lab43.com>
2021-01-26 18:27:18 -0800

    add parent class and custom wrapper tag to activeLink helper

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+const renderAttributes = (attributes) => {
+  const output = [];
+  for (attribute in attributes) {
+    output.push(` ${attribute}="${attributes[attribute]}"`);
+  }
+  return output.join('');
+}
+
+
+
 module.exports = () => ({ registerTemplateHelper }) => {
 
-  registerTemplateHelper('activeLink', (metadata, href, title, attributes = {}) => {
+  registerTemplateHelper('activeLink', (metadata, href, title, attributes = {}, wrapper) => {
     // if this link goes to the current page add the class "active"
     if ('/' + metadata.slug === href) {
       attributes.class = attributes.class ? attributes.class += ' active' : 'active';
     }
-    // construct and return the link
-    const output = [`<a href="${href}"`];
-    for (attribute in attributes) {
-      output.push(` ${attribute}="${attributes[attribute]}"`);
+    // if this page is a child of the link add the class "parent"
+    if (('/' + metadata.slug).startsWith(href)) {
+      attributes.class = attributes.class ? attributes.class += ' parent' : 'parent';
     }
-    output.push(`>${title}</a>`);
+    // construct and return the link
+    const output = [];
+    if (wrapper) {
+      output.push(`<${wrapper}`);
+      output.push(renderAttributes(attributes));
+      output.push(`><a href="${href}">`);
+    } else {
+      output.push(`<a href="${href}"`);
+      output.push(renderAttributes(attributes));
+      output.push('>');
+    }
+    output.push(title);
+    if (wrapper) {
+      output.push(`</a></${wrapper}>`);
+    } else {
+      output.push('</a>');
+    }
     return output.join('');
   });
 
